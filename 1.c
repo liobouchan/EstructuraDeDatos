4e7cdd9129827291652f43f10530b5db1504dcce
liobouchan <leo.bouchan@gmail.com>
2014-02-04 23:16:06 -0600

    Practica en clase de Estructura de datos, el usuario ingresa los datos a la estructura

---
+++ 1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Persona{
+    char nombre[30];
+    char Ap[30];
+    char Am[30];
+    int edad;
+    char sexo;
+    char nacionalidad[50];
+    char direccion[100];
+    char CURP[30];
+}Persona;
+
+int main(){
+    Persona humano;
+    int edad = 0;
+    char sex;
+
+    printf("%s\n", "Definir persona" );
+    printf("%s\n", "Nombre: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    gets(humano.nombre);
+
+    printf("%s\n", "Apellido Paterno: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    gets(humano.Ap);
+
+    printf("%s\n", "Apellido Materno: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    gets(humano.Am);
+
+    printf("%s\n", "Edad: " );
+    fflush(stdin);
+    scanf("%d", &edad);
+    humano.edad = edad;
+
+    printf("%s\n", "Sexo: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    scanf("%c", &sex);
+    humano.sexo = sex;
+
+    printf("%s\n", "Nacionalidad: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    gets(humano.nacionalidad);
+
+    printf("%s\n", "Direccion: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    gets(humano.direccion);
+
+    printf("%s\n", "CURP: " );
+    fflush(stdin);
+    setbuf(stdin,0);
+    gets(humano.CURP);
+
+    printf("%s\n", "Los datos ingresados fueron" );
+    printf("%s\n", humano.Ap );
+    printf("%s\n", humano.nombre );
+    printf("%d\n", humano.edad );
+    printf("%c\n", humano.sexo );
+    printf("%s\n", humano.nacionalidad );
+    printf("%s\n", humano.direccion );
+    printf("%s\n", humano.CURP );
+
+}
