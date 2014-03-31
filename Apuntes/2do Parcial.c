a68172c8d70086c6f57f7c3d8bbb98f092031f23
LioBouchan <leo.bouchan@gmail.com>
2014-03-31 17:23:13 -0600

    Apuntes segundo parcial

---
+++ Apuntes/2do Parcial.c
@@ -0,0 +1,21 @@
+//Pilas
+	Es un tipo de dato abstracto lineal de elementos del mismo tipo, dónde cada
+	elemento se puede agregar o quitar únicamente por uno de sus extremos.
+
+  En consecuencia, los elementos de la pila se eliminan en orfen inverso al que
+  se insertaron, es decir , "el último elemento en entrar, es el primero en
+  salir".
+  
+  Debido a esto, a una pila se le conoce como una estructura LIFO (Last-in , First-out).
+
+  La pila se puede definir como una colección de datos a los cuales se puede
+  acceder por un extremo llamado tope.
+
+  //Representación de la pila
+    Una pila se puede representar de dos formas:
+      1) Estática.- Utilizando un arreglo
+      2) Dinámica - Utilizando una lista.
+
+  //Tope 
+    1; El lugar donde se va a insertar un elemento en la pila.
+    2; El elemento que se va a eliminar de la pila.
